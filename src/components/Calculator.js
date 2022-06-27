2a9e3d9e784e3b506b4a059c0c7be39613d1cb81
Alfred Boateng <programmingwithalfred@gmail.com>
2022-06-27 21:25:17 +0000

    Refactor Calculator.js to use hooks
    React class components replaced with react hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -5,110 +5,104 @@ import calculate from '../logic/calculate';
 import './Calculator.css';
 import Button from './button';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      obj: { total: null, next: null, operation: null },
-    };
+const Calculator = () => {
+  const [obj, setObj] = React.useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+  const handleClick = (buttonName) => {
+    setObj((prevState) => {
+      const results = {
+        ...prevState,
+        ...calculate(prevState, buttonName),
+      };
+      return results;
+    });
+  };
 
-  handleClick(buttonName) {
-    this.setState((state) => ({
-      obj: {
-        ...state.obj,
-        ...calculate(state.obj, buttonName),
-      },
-    }));
-  }
+  const results = obj.next ? obj.next : obj.total;
+  const rowStyles = {
+    width: '100%',
+    color: 'black',
+    display: 'flex',
+    flexWrap: 'nowrap',
+  };
 
-  render() {
-    const { obj } = this.state;
-    const results = obj.next ? obj.next : obj.total;
+  const containerStyles = {
+    width: '500px',
+  };
 
-    const rowStyles = {
-      width: '100%',
-      color: 'black',
-      display: 'flex',
-      flexWrap: 'nowrap',
-    };
+  const orangeColor = '#F5913E';
 
-    const containerStyles = {
-      width: '500px',
-    };
-
-    const orangeColor = '#F5913E';
-
-    return (
-      <>
-        <div style={containerStyles}>
-          <div style={rowStyles}>
-            <InputElement
-              width="100%"
-              backgroundColor="#858694"
-              value={results}
-              elementPosition="flex-end"
-            />
-          </div>
-          <div style={rowStyles}>
-            <Button text="AC" handleClick={this.handleClick} />
-            <Button handleClick={this.handleClick} text="+/-" />
-            <Button handleClick={this.handleClick} text="%" />
-            <Button
-              handleClick={this.handleClick}
-              text="÷"
-              backgroundColor={orangeColor}
-            />
-          </div>
-
-          <div style={rowStyles}>
-            <Button handleClick={this.handleClick} text="7" />
-            <Button handleClick={this.handleClick} text="8" />
-            <Button handleClick={this.handleClick} text="9" />
-            <Button
-              handleClick={this.handleClick}
-              text="x"
-              backgroundColor={orangeColor}
-            />
-          </div>
-
-          <div style={rowStyles}>
-            <Button handleClick={this.handleClick} text="4" />
-            <Button handleClick={this.handleClick} text="5" />
-            <Button handleClick={this.handleClick} text="6" />
-            <Button
-              handleClick={this.handleClick}
-              text="-"
-              backgroundColor={orangeColor}
-            />
-          </div>
-
-          <div style={rowStyles}>
-            <Button handleClick={this.handleClick} text="1" />
-            <Button handleClick={this.handleClick} text="2" />
-            <Button handleClick={this.handleClick} text="3" />
-            <Button
-              handleClick={this.handleClick}
-              text="+"
-              backgroundColor={orangeColor}
-            />
-          </div>
-
-          <div style={rowStyles}>
-            <Button handleClick={this.handleClick} text="0" width="51%" />
-            <Button handleClick={this.handleClick} text="." />
-            <Button
-              handleClick={this.handleClick}
-              text="="
-              backgroundColor={orangeColor}
-            />
-          </div>
+  return (
+    <>
+      <div style={containerStyles}>
+        <div style={rowStyles}>
+          <InputElement
+            width="100%"
+            backgroundColor="#858694"
+            value={results}
+            elementPosition="flex-end"
+          />
         </div>
-      </>
-    );
-  }
-}
+        <div style={rowStyles}>
+          <Button text="AC" handleClick={handleClick} />
+          <Button handleClick={handleClick} text="+/-" />
+          <Button handleClick={handleClick} text="%" />
+          <Button
+            handleClick={handleClick}
+            text="÷"
+            backgroundColor={orangeColor}
+          />
+        </div>
+
+        <div style={rowStyles}>
+          <Button handleClick={handleClick} text="7" />
+          <Button handleClick={handleClick} text="8" />
+          <Button handleClick={handleClick} text="9" />
+          <Button
+            handleClick={handleClick}
+            text="x"
+            backgroundColor={orangeColor}
+          />
+        </div>
+
+        <div style={rowStyles}>
+          <Button handleClick={handleClick} text="4" />
+          <Button handleClick={handleClick} text="5" />
+          <Button handleClick={handleClick} text="6" />
+          <Button
+            handleClick={handleClick}
+            text="-"
+            backgroundColor={orangeColor}
+          />
+        </div>
+
+        <div style={rowStyles}>
+          <Button handleClick={handleClick} text="1" />
+          <Button handleClick={handleClick} text="2" />
+          <Button handleClick={handleClick} text="3" />
+          <Button
+            handleClick={handleClick}
+            text="+"
+            backgroundColor={orangeColor}
+          />
+        </div>
+
+        <div style={rowStyles}>
+          <Button handleClick={handleClick} text="0" width="51%" />
+          <Button handleClick={handleClick} text="." />
+          <Button
+            handleClick={handleClick}
+            text="="
+            backgroundColor={orangeColor}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Calculator;
