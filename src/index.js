c052125467b29811ee8ad76e8ecb26956fc7ca77
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-03 18:21:40 +0100

    Add router to pages
    Three routes created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import HomePage from './routes/Home';
+import QuotesPage from './routes/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="quotes" element={<QuotesPage />} />
+      <Route path="maths" element={<App />} />
+    </Routes>
   </BrowserRouter>,
 );
---
+++ src/routes/Quote.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+
+function QuotesPage() {
+  return (
+    <>
+      <Navbar />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi
+        laborum animi pariatur eveniet quam sint culpa saepe fuga, expedita ipsa
+        omnis sapiente consectetur hic libero iste id? Et, voluptas nisi.
+      </p>
+
+      <Footer />
+    </>
+  );
+}
+export default QuotesPage;
