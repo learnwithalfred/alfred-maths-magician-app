437af72d32a7750f8d865665c38f400a17fd8d40
Alfred Boateng <programmingwithalfred@gmail.com>
2022-07-03 17:38:19 +0100

    Add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
   return (
-    <div>
-      <span>footer goes here</span>
-      <p>and social links are also here</p>
-    </div>
+    <footer className="footer">
+      <span>
+        Created with love by
+        <a
+          href="github.com/learnwithalfred/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          alfred boateng
+        </a>
+      </span>
+    </footer>
   );
 }
 
